76bed1fe0181c49b5ee28fa69e5d684d6a53b76b
XHwind <310381979@qq.com>
2021-05-30 23:35:51 -0700

    update

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 Repository for CSE257's Final Project
+
+## Setup the environment
+
+Please follow the original [sgm](https://github.com/XHwind/cse257_final_pj/tree/main/sgm) install instructions. And replace the `evaluation.py` and `planner.py` and `simple_navigation.py` with the one in the top folder. Those files are modified to support different functionality. 
+
+## Running instructions
+
+To directly tun the experiment, please run
+
+```bash
+python main.py
+```
+
+To use jupyter-notebook, please see the `exp.iphynb` and save the plots in the disired path. To generate simle plots and gif, please run 
+
+```bash
+python myplot.py
+```
+
+The GIF for the SGM with different cleanup steps are shown below.
+
+![1](./img/1.gif)
--- myplot.py
+++ myplot.py
@@ -2,8 +2,21 @@ import numpy as np
 import pandas as pd
 import pdb
 import matplotlib.pyplot as plt
+
+import imageio
+import os
+def generate_gif(path):
+    files = os.listdir(path)
+    imgs = []
+    paths = [os.path.join(path,file) for file in files if file[-3:] == "png"]
+    paths = sorted(paths, key=lambda x: int(x.split("/")[-1][:-4]))
+    for path in paths:
+        imgs.append(plt.imread(path))
+    kargs = { 'duration': 0.3 }
+    imageio.mimsave("1.gif", imgs, "GIF", **kargs)
 if __name__=="__main__":
     data = pd.read_csv("/home/xh/pj/CSE257/temp/sgm/logs/thinned_fourrooms_sgm_2021-05-31_02-44-29/evaluation.csv")
     plt.plot(data["Cleanup Steps"].to_numpy()[1:], data["Success Rate"].to_numpy()[1:])
     plt.show()
-    pdb.set_trace()
+    path_to_images="./"
+    generate_gif(path_to_images)
